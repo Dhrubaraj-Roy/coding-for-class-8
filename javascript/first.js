906071e45a17ad760f1ddc7422782546b4790825
Dhrubaraj-Roy <droy@datawars.io>
2026-07-18 12:22:58 +0530

    non primitive datatype

--- javascript/first.js
+++ javascript/first.js
@@ -21,3 +21,36 @@ console.log(c);
 
 let d = 1234567890123456789012345678901234567890n ;
 console.log(d);
+
+
+
+//non primitive data type 
+//Array, Object, Function
+
+
+//object
+let arr = [12,34,66, "Dhruba", "ram"];
+console.log(arr);
+console.log(typeof arr);
+
+
+//object 
+//key:value
+
+let obj = {
+  name: "Dhruba",
+  age: 18,
+  city: "Kathmandu",
+  cof: "Dhananjay Roy"
+};
+console.log(obj);
+console.log(typeof obj);
+
+//function
+let fun = function(){
+    console.log("Hello word")
+}
+
+fun();
+console.log(typeof fun) // function 
+
