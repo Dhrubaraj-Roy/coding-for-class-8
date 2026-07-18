37058345aee5b0db92cb731505969dfb0cb886f1
Dhrubaraj-Roy <droy@datawars.io>
2026-07-18 12:49:15 +0530

    type conversion etc

--- javascript/first.js
+++ javascript/first.js
@@ -54,3 +54,49 @@ let fun = function(){
 fun();
 console.log(typeof fun) // function 
 
+
+//Type conversion 
+console.log("Type conversion ------------------")
+
+let acc_b = "1000";
+console.log(typeof acc_b);
+
+let num_acc = Number(acc_b);
+console.log(num_acc);
+console.log(typeof num_acc);    
+
+//boolen convert to number
+
+let x = true
+console.log(Number(x));
+
+
+let random = "100ere"
+
+console.log(Number(random)) //NaN - the full form of NaN is Not a Number 
+
+//Null
+let random2 = null
+console.log(Number(random2))
+
+//Undefined 
+let random3 = undefined
+console.log(Number(random3))
+
+
+//Arithmetic Operators
+
+console.log("Arithmetic Operators  --------")
+
+console.log(10 + 20)
+console.log(10 - 20)
+console.log(10 * 20)
+console.log(10 / 20)
+console.log(10 % 20)
+
+//1st - Divide and multiply left to right
+//2nd - Add and subtract left to right
+
+console.log(22/4+34*2-44);
+
+
