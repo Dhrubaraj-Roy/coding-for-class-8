a5bd8047c7a65f4dde89cc4b6c8ba99ae1b27292
Dhrubaraj-Roy <droy@datawars.io>
2026-08-05 18:05:50 +0530

    select element by attribute

--- javascript/this_keyword.js
+++ javascript/this_keyword.js
@@ -6,4 +6,24 @@
 
 // console.log(this.document)
 
-document.getElementById("first").innerHTML = "You are working with DOM ok ";
+// document.getElementById("first").innerHTML = "You are working with DOM ok ";
+
+// function attach(content){
+//     const element = document.createElement("li");
+//     element.textContent = content;
+
+//     const list = document.getElementById("third");
+//     list.appendChild(element);
+
+// }
+
+// attach("Git and GitHub");
+// attach("React");
+// attach("Backend");
+
+
+const UL = document.querySelector('ul');
+UL.setAttribute("class", "Dhruba");
+UL.removeAttribute("class", "Dhruba");
+
+console.log(UL.getAttribute("class"));
