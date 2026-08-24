e90a09289009d2c0915e914fae134474fb7a6123
Dhrubaraj-Roy <droy@datawars.io>
2026-08-24 23:26:40 +0530

    day29

---
+++ javascript/Day29/script.js
@@ -0,0 +1,33 @@
+// console.log('hello Dhruba');
+
+
+// function meet(){
+//     const arr = [1,2,3,4];
+//     console.log(arr[0]);
+// }
+
+// function greet(){
+//     const a = 2+4;
+//     console.log(a);
+//     meet();
+//     console.log(a*a);
+
+// }
+
+// greet();
+
+// console.log('program end')
+
+
+console.log("hello coder...")
+setTimeout(() => {
+    const a = 2+4;
+    console.log(a)
+    
+}, 5000);
+
+let b = 20;
+let arr = [20,30,11];
+for(let i of arr){
+    console.log(i*b);
+}
