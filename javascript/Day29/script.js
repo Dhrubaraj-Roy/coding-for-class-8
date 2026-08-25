e76ea269038265bf33c9839fcf67a5dc4331ec9b
Dhrubaraj-Roy <droy@datawars.io>
2026-08-25 10:46:17 +0530

    day29 complete

--- javascript/Day29/script.js
+++ javascript/Day29/script.js
@@ -19,15 +19,38 @@
 // console.log('program end')
 
 
-console.log("hello coder...")
-setTimeout(() => {
-    const a = 2+4;
-    console.log(a)
+// console.log("hello coder...")
+// setTimeout(() => {
+//     const a = 2+4;
+//     console.log(a)
     
-}, 5000);
+// }, 5000);
 
-let b = 20;
-let arr = [20,30,11];
-for(let i of arr){
-    console.log(i*b);
-}
+// let b = 20;
+// let arr = [20,30,11];
+// for(let i of arr){
+//     console.log(i*b);
+// }
+
+// console.log("hello coder...")
+// setTimeout(() => {
+//     let a = 2+4;
+//     console.log(a)
+// }, 5000);
+
+// setInterval(() => {
+//     console.log("I am fast")
+// }, 2000);
+
+// let b = 10;
+// let arr = [20,30,11];
+// for(let i of arr){
+//     console.log(i*b);
+// }
+
+
+console.log("I am fast");
+
+fetch("https://youtube.com").then(()=>console.log("hello"));
+console.log("I am last");
+
