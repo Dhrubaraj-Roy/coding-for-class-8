64dbf28792fec3000f2e1909106dd493429f2980
Dhrubaraj-Roy <dhrubarajroy123@gmail.com>
2026-08-30 14:03:16 +0530

    typescript1

--- typescript/app.js
+++ typescript/app.js
@@ -1,3 +1,9 @@
 "use strict";
 let a = 10;
 let b = 20;
+//string 
+let str = "Hello World";
+//boolen
+let isTrue = true;
+let names = "Dhruba";
+let age = 24;
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,2 +1,12 @@
 let a:number = 10;
 let b:number = 20;
+
+//string 
+let str:string = "Hello World";
+
+//boolen
+let isTrue:boolean = true;
+
+let names:string = "Dhruba";
+let age:number = 24;
+
