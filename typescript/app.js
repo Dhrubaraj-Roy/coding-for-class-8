d3b40e262a75ddc7e61155b0dc41c32cd677e6eb
Dhrubaraj-Roy <dhrubarajroy123@gmail.com>
2026-08-30 20:10:08 +0530

    typescript2

--- typescript/app.js
+++ typescript/app.js
@@ -7,3 +7,29 @@ let str = "Hello World";
 let isTrue = true;
 let names = "Dhruba";
 let age = 24;
+let vill;
+vill = "gokulnagar";
+vill = 12;
+let val;
+val = 12;
+val = "hello";
+//array
+let arr = [1, 2, 3, 4, 5];
+let arr2 = [1, 2, 3, 4, 5, "hello", "world", true, false];
+//tuple
+// let arr3:[string,number,boolean] = ["hello", 12, 14]; so it will gives me error
+let arr4 = ["hello", 12, true]; //correct syntax
+//object
+//inline syntax
+let obj = {
+    name: "Dhruba",
+    age: 24,
+    gender: "male"
+};
+//way2
+let person;
+person = { name: "Shubha", age: 24, gender: "male" };
+let person1 = { name: "Ram", age: 24, gender: "male" };
+console.log(person1.name);
+let person2 = { name: "Shyam", age: 24, id: 1, gender: "male" };
+console.log(person2);
--- typescript/app.ts
+++ typescript/app.ts
@@ -10,3 +10,63 @@ let isTrue:boolean = true;
 let names:string = "Dhruba";
 let age:number = 24;
 
+
+let vill:any;
+vill = "gokulnagar";
+vill = 12;
+
+let val:unknown;
+val = 12;
+val = "hello";
+
+//array
+let arr:number[] = [1,2,3,4,5];
+
+let arr2:(string|number|boolean)[] = [1,2,3,4,5,"hello","world", true,false];
+
+
+//tuple
+// let arr3:[string,number,boolean] = ["hello", 12, 14]; so it will gives me error
+let arr4:[string,number,boolean] = ["hello", 12, true]; //correct syntax
+
+
+//object
+
+//inline syntax
+let obj:{name:string, age:number, gender:string} = {
+    name:"Dhruba",
+    age:24,
+    gender:"male"
+}
+
+//way2
+let person:{name:string, age:number, gender:string};
+
+person = {name:"Shubha", age:24, gender:"male"};
+
+
+//way3 for clean code
+type Person = {
+    name:string,
+    age:number,
+    gender:string
+}
+
+let person1:Person = {name:"Ram", age:24, gender:"male"};
+console.log(person1.name);
+
+ 
+
+
+interface admin {
+    name:string,
+    age:number,
+    gender:string
+}  
+
+interface admin {   
+    id:number
+}
+
+let person2:admin = {name:"Shyam", age:24,  id:1, gender:"male"};    
+console.log(person2);
