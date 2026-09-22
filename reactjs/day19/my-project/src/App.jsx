f30045361e380a35235cd18c80f4d5b83eed6f28
Dhrubaraj-Roy <dhrubarajroy123@gmail.com>
2026-09-22 22:58:33 +0530

    day 19 of tailwind css

---
+++ reactjs/day19/my-project/src/App.jsx
@@ -0,0 +1,99 @@
+
+function App() {
+
+  return (
+    <div className="flex flex-wrap justify-center gap-1.5"> <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+     <div className="bg-gray-500 w-fit m-5 rounded-2xl ">
+    <div className="max-w-sm">
+      <img  className="w-full h-80 overflow-hidden object-cover rounded-2xl" src="https://i.pinimg.com/1200x/26/af/11/26af11ff5c51f9cf0b094d167b8748cf.jpg" alt="Comatazz" />
+    </div>
+    <div className="max-w-sm">
+      <h2 className="mt-3 ml-2 text-3xl">Biography</h2>
+      <p className="text-sm mt-1 ml-2 text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, voluptatibus?</p>
+    </div>
+    <button className="mt-1 mb-2 ml-2 font-bold">Know More</button>
+    </div>
+    </div>
+  )
+}
+
+export default App
